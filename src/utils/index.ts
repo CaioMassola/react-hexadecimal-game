43520c3648602faa8830a419ee9bdcb2715d0c36
CaioMassola <caio.massola@outlook.com>
2023-04-03 16:52:57 -0300

    implements: layout game

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,27 +1,25 @@
 const randomColor = (): string => {
-
-    const values: string = '0123456789ABCDEF';
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-        color += values[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  const values: string = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += values[Math.floor(Math.random() * 16)];
+  }
+  return color;
 };
 
 const getColors = (): string[] => {
-    const colors: string[] = [];
-    while (colors.length < 3) {
-        const color = randomColor();
-        if (!colors.includes(color)) {
-            colors.push(color);
-        }
+  const colors: string[] = [];
+  while (colors.length < 3) {
+    const color = randomColor();
+    if (!colors.includes(color)) {
+      colors.push(color);
     }
-    return colors;
+  }
+  return colors;
 };
 
 const colorOption = (colors: string[]) => {
-    return colors[Math.floor(Math.random() * colors.length)];
-}
+  return colors[Math.floor(Math.random() * colors.length)];
+};
 
-
-export { getColors, colorOption }
+export { getColors, colorOption };
